49507eafd4660dcb0ed357f4f284467ecde0e136
Tanay Vidhate <tanayvidhate05@gmail.com>
2026-01-04 20:16:15 +0530

    android fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.astrolab', // IMPORTANT: Change this to a unique ID for your app
+  appId: 'com.warlord05.astrolab',
   appName: 'Astrolab',
   webDir: 'dist', // This points to your Vite build output directory
   bundledWebRuntime: false
